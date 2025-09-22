92a2ce6d02b1c8ace207eea78ab9dc083ba514ac
Pavan Karthik Kokkiligadda <pavankarthik1980@gmail.com>
2025-09-22 21:42:56 +0000

    Try fixing this error: `Runtime Error: Error: notFound() is not allowed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,6 @@ import { cn } from '@/lib/utils';
 import 'leaflet/dist/leaflet.css';
 import { Header } from '@/components/layout/header';
 import Image from 'next/image';
-import {notFound} from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Voice2Action',
@@ -20,9 +19,6 @@ export default function RootLayout({
   children: React.ReactNode;
   params: {locale: string};
 }>) {
-  // Validate that the incoming `locale` parameter is valid
-  const locales = ['en', 'hi', 'bn', 'te', 'ta', 'mr', 'gu', 'kn', 'ml', 'pa', 'ur'];
-  if (!locales.includes(locale)) notFound();
 
   return (
     <html lang={locale} suppressHydrationWarning>
