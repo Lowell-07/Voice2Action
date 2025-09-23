73b92e1fcc33d52c3ded797d4f6d3b3c046fb0f9
Pavan Karthik Kokkiligadda <pavankarthik1980@gmail.com>
2025-09-23 02:54:39 +0000

    where is my reported problem snap shot of data which must be redirecting

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -53,7 +53,7 @@ export default function ProfilePage() {
     );
   }
   
-  const userProblems = problems.filter(p => p.reportedById === user.data.id);
+  const userProblems = problems.filter(p => p.reportedBy.id.toString() === user.data.id.toString());
   const resolvedProblems = userProblems.filter(p => p.status === 'Resolved').length;
   const recentProblems = userProblems.slice(0, 3);
 
